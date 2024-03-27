72b114c80e10d9e831364338b6c60ced4cd4110c
minim0no <ahmed.idrees3110@gmail.com>
2024-03-27 02:05:47 -0500

    created functions to manage credential storage for client id and client secret

---
+++ src/commands/login.ts
@@ -0,0 +1,21 @@
+import { Command } from "commander";
+
+const login = new Command("login")
+    .description("Login to the REST API to begin using the PayPal CLI")
+    .argument("<string>", "Client ID")
+    .argument("<string>", "Client Secret")
+    .action((clientId, clientSecret) => {});
+
+// --help
+login.addHelpText(
+    "after",
+    `
+    
+    To get a Client ID and Client Secret, follow these steps:
+        1. Log into the developer dashboard @ https://developer.paypal.com/dashboard/
+        2. Select Apps & Credentials.
+        3. New accounts come with a Default Application in the REST API apps section. To create a new project, select Create App.
+        4. Copy the client ID and client secret for your app.
+
+    `
+);
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,25 +1,71 @@
-import { request } from "http";
+import { setPassword, getPassword } from "keytar";
 
-function auth(clientId: string, clientSecret: string) {
-    let access_token = "";
-
+export async function auth(clientId: string, clientSecret: string) {
     const authorization =
         "Basic " +
         Buffer.from(clientId + ":" + clientSecret).toString("base64"); // PayPal API wants Base64
 
-    fetch("https://api-m.sandbox.paypal.com/v1/oauth2/token", {
-        method: "POST",
-        headers: {
-            Authorization: authorization,
-            Accept: "application/json",
-            "Accept-Language": "en_US",
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: "grant_type=client_credentials",
-    })
-        .then((response) => response.json())
-        .then((data) => (access_token = data.access_token))
-        .catch((error) => console.error("Error:", error));
+    const response = await fetch(
+        "https://api-m.sandbox.paypal.com/v1/oauth2/token",
+        {
+            method: "POST",
+            headers: {
+                Authorization: authorization,
+                Accept: "application/json",
+                "Accept-Language": "en_US",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: "grant_type=client_credentials",
+        }
+    ).catch((error) => console.error("Error:", error));
 
-    return access_token;
+    const data = await (response as Response).json();
+
+    return data.access_token;
+}
+
+export async function setClientId(clientId: string) {
+    try {
+        await setPassword("PayPal", "ClientId", clientId);
+        console.log("Client ID saved successfully.");
+    } catch (error) {
+        console.error("Error saving Client ID: ", error);
+    }
+}
+
+export async function setClientSecret(clientSecret: string) {
+    try {
+        await setPassword("PayPal", "ClientSecret", clientSecret);
+        console.log("Client Secret saved successfully.");
+    } catch (error) {
+        console.error("Error saving Client Secret: ", error);
+    }
+}
+
+export async function getClientId() {
+    try {
+        const clientId = await getPassword("PayPal", "ClientId");
+        if (clientId) {
+            console.log("Client ID retrieved successfully.");
+            return clientId;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching Client ID: ", error);
+    }
+}
+
+export async function getClientSecret() {
+    try {
+        const clientSecret = await getPassword("PayPal", "ClientSecret");
+        if (clientSecret) {
+            console.log("Client Secret retrieved successfully.");
+            return clientSecret;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching Client Secret: ", error);
+    }
 }
